1da86c18c3c3e5f09b04a9e25420fa6fd84a37be
Gleb_V.Mikhaylov <migleb@yandex.ru>
2021-10-11 21:23:54 +0300

    lesson3 edits

--- myweb/myweb.c
+++ myweb/myweb.c
@@ -5,6 +5,8 @@
 #define HTTP_REQUEST_LEN 256
 #define HTTP_METHOD_LEN 6
 #define HTTP_URI_LEN 100
+#define HTTP_URI_PATH_LEN 40
+#define HTTP_URI_PARAM_LEN 40
 
 #define REQ_END 100
 #define ERR_NO_URI -100
@@ -14,8 +16,8 @@ struct http_req {
 	char request[HTTP_REQUEST_LEN];
 	char method[HTTP_METHOD_LEN];
 	char uri[HTTP_URI_LEN];
-	// uri_path
-	// uri_params
+	char uri_path[HTTP_URI_PATH_LEN];
+	char uri_params[HTTP_URI_PARAM_LEN];
 	// version
 	// user_agent
 	// server
@@ -28,6 +30,9 @@ int fill_req(char *buf, struct http_req *req) {
 		return REQ_END;
 	}
 	char *p, *a, *b;
+	char* p_up = 0; //Путь (uri path)
+	char* p_upl = 0; //Строка параметров (uri_params)
+
 	// Это строка GET-запроса
 	p = strstr(buf, "GET");
 	if (p == buf) {
@@ -39,20 +44,30 @@ int fill_req(char *buf, struct http_req *req) {
 		strncpy(req->request, buf, strlen(buf));
 		strncpy(req->method, "GET", strlen("GET"));
 		a = strchr(buf, '/');
-		if ( a != NULL) { // есть запрашиваемый URI 
+		if ( a != NULL) 
+		{ // есть запрашиваемый URI 
 			b = strchr(a, ' ');
-			if ( b != NULL ) { // конец URI
+			if ( b != NULL )
+			{ // конец URI
 				strncpy(req->uri, a, b-a);
-			} else {
-				return ERR_ENDLESS_URI;  
+				p_up = strchr(a, '?');
+				if( p_up != NULL )
+				{
+					strncpy(req->uri_path, a, p_up - a);
+				}
+				strncpy(req->uri_params, p_up, b - p_up);
+			} 
+			else 
+			{
+				return ERR_ENDLESS_URI;
 				// тогда это что-то не то
 			}
-		} else {
+		}
+		else {
 			return ERR_NO_URI; 
 			// тогда это что-то не то
 		}
 	}
-
 	return 0;	
 }
 
@@ -65,25 +80,31 @@ int make_resp(struct http_req *req) {
 	printf("HTTP/1.1 200 OK\r\n");
 	printf("Content-Type: text/html\r\n");
 	printf("\r\n");
-	printf("<html><body><title>Page title</title><h1>Page Header</h1></doby></html>\r\n");
+	printf("<html><body><title>Page title</title><h1>Page HeaderTitle2</h1></body></html>\r\n");
+
+	printf("<p>Request:  %s</p>", req->request);
+	printf("<p>Method: %s</p>", req->method);
+	printf("<p>Uri: %s</p>", req->uri);
+	printf("<p>Uri_path: %s</p>", req->uri_path);
+	printf("<p>Uri_params: %s</p>", req->uri_params);
+
 	return 0;
 }
 
 int main (void) {
 	char buf[HTTP_HEADER_LEN];
-	struct http_req req;
+	struct http_req req = {0};
 	while(fgets(buf, sizeof(buf),stdin)) {
 		int ret = fill_req(buf, &req);
-		if (ret == 0) 
+		if (ret == 0)
 			// строка запроса обработана, переходим к следующей
 			continue;
-		if (ret == REQ_END ) 
+		if (ret == REQ_END )
 			// конец HTTP запроса, вываливаемся на обработку
 			break;
 		else
-			// какая-то ошибка 
+			// какая-то ошибка
 			printf("Error: %d\n", ret);
-		
 	}
 	log_req(&req);
 	make_resp(&req);
